f9c49099ead14498082f29d447ea522da7cd5ca5
Lambert <lambert.muenster@gmail.com>
2026-06-12 11:50:43 +0200

    some cleaning of directory

--- simulation/obj/lattice/lattice.d
+++
@@ -1,2 +0,0 @@
-obj/lattice/lattice.d obj/lattice/lattice.o: src/lattice/lattice.cpp \
- src/lattice/lattice.h
--- simulation/obj/random_numbers/random_numbers.d
+++
@@ -1,4 +0,0 @@
-obj/random_numbers/random_numbers.d obj/random_numbers/random_numbers.o: \
- src/random_numbers/random_numbers.cpp \
- src/random_numbers/random_numbers.h \
- src/random_numbers/../lattice/lattice.h
